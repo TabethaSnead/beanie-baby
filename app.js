e3ccd71f07d1a2fc3e6a9ef5887c7391cfd6ed51
TabethaSnead <tabetha.snead@gmail.com>
2023-01-20 13:00:30 -0800

    called getAllBeanies and console logged all beanies

--- app.js
+++ app.js
@@ -11,5 +11,19 @@ let beanieZodiac = [];
 /* Events */
 
 /* Display Functions */
+async function getAllBeanies() {
+    //fetch all beanies
+    const beanies = await findAllBeanies();
+    //set beanies to state
+    console.log(beanies);
+    //display all beanies
+}
+getAllBeanies();
+function displayBeanies() {
+    //reset display
+    //loop through beanies array
+    // for each beanie send to render function that creates li
+    //append
+}
 
 // (don't forget to call any display functions you want to run on page load!)
